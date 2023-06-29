435332cb02edf8adcb2e53e39ca61a39491264f6
leoziondev <leozionsuporte@gmail.com>
2023-06-29 17:16:30 -0300

    get field values

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -24,6 +24,7 @@ const YoutubehtmlForm = () => {
         control,
         handleSubmit,
         watch,
+        getValues,
         formState: { errors }
     } = useForm<FormValues>({
         defaultValues: {
@@ -51,16 +52,21 @@ const YoutubehtmlForm = () => {
         
     }
 
+    const handleGetValues = () => {
+        // console.log("Get values: ", getValues("social"))
+        console.log("Get values: ", getValues(["username","email", "social"]))
+    }
+
     // const watchUsername = watch('username')
     // const watchForm = watch()
     // console.log(watchForm)
 
-    useEffect(() => {
-        const subscription = watch((value) => {
-            console.log(value)
-        })
-        return () => subscription.unsubscribe()
-    }, [watch])
+    // useEffect(() => {
+    //     const subscription = watch((value) => {
+    //         console.log(value)
+    //     })
+    //     return () => subscription.unsubscribe()
+    // }, [watch])
 
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -269,6 +275,13 @@ const YoutubehtmlForm = () => {
 
                 <div>
                     <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
+                    <button
+                        type="button"
+                        onClick={handleGetValues}
+                        className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-4"
+                        >
+                            Get Values
+                        </button>
                 </div>
             </form>
         </div>
